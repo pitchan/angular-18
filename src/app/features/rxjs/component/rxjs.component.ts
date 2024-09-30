66f6ebc1df0fb31ac27db1526e0f6239648544c0
pitchan <vincent.cueto@gmail.com>
2024-09-30 22:37:07 +0200

    Fix suggestion box position

--- src/app/features/rxjs/component/rxjs.component.ts
+++ src/app/features/rxjs/component/rxjs.component.ts
@@ -14,7 +14,7 @@ import { RxjsService } from '../services/rxjs.service';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { catchError, concatMap, debounceTime, distinctUntilChanged, exhaustMap, filter, map, mergeMap, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { EMPTY, merge, of } from 'rxjs';
 
 @Component({
@@ -31,51 +31,29 @@ export class RxjsComponent {
   readonly autoCompleteConcatMapControl = new FormControl();
   readonly autoCompleteMergeMapControl = new FormControl();
   readonly autoCompleteExhaustMapControl = new FormControl();
-  readonly delayControl = new FormControl();
-
   
   loading = signal<boolean>(false);
   logMessages = signal<{message: string, type: 'info' | 'success'}[]>([]);
 
   selectedPokemon = signal<Pokemon | null>(null);  // Signal pour le Pokémon sélectionné
-  filteredPokemons = signal<Pokemon[]>([]);        // Signal pour la liste des Pokémon filtrés
-  isDelay = signal<boolean>(false);
-  filteredPokemons$ = merge(
-    this.initAutocompleteSwitchMapObservable(),
-    this.initAutocompleteConcatMapObservable(),
-    this.initAutocompleteMergeMapObservable(),
-    this.initAutocompleteExhaustMapObservable(),
-  );
+  isDelay = false;
+  
+  filteredPokemonsSwitchMap$ = this.initAutocompleteSwitchMapObservable();
+  filteredPokemonsConcatMap$ = this.initAutocompleteConcatMapObservable();
+  filteredPokemonsMergeMap$ = this.initAutocompleteMergeMapObservable();
+  filteredPokemonsExhaustMap$ = this.initAutocompleteExhaustMapObservable();
 
   selectedOperator = 'switchMap';
   operatorControl = new FormControl('switchMap'); // Valeur initiale
+  
 
   constructor() {}
-
-  /*loadRandomPokemon() {
-    this.loading.set(true);
-    this.error.set('');
-
-    const randomId = Math.floor(Math.random() * 898) + 1; // ID entre 1 et 898
-
-    this.pokemonApiService.getPokemon(randomId.toString()).subscribe({
-      next: (data) => {
-        console.log(data);
-        this.selectedPokemon.set(data);
-        this.loading.set(false);
-      },
-      error: (err) => {
-        this.error.set('Erreur lors du chargement du Pokémon.');
-        this.loading.set(false);
-      },
-    });
-  } */
-
-  
+ 
 
   initAutocompleteSwitchMapObservable() {
     return this.autoCompleteSwitchMapControl.valueChanges
       .pipe(
+        takeUntilDestroyed(),
         filter((value: string | null): value is string => value !== null && value.length > 0),
         //debounceTime(300),
         //distinctUntilChanged(),    
@@ -91,6 +69,7 @@ export class RxjsComponent {
   initAutocompleteConcatMapObservable() {
     return this.autoCompleteConcatMapControl.valueChanges
       .pipe(
+        takeUntilDestroyed(),
         filter((value: string | null): value is string => value !== null && value.length > 0),
         //debounceTime(300),
         //distinctUntilChanged(),        
@@ -106,6 +85,7 @@ export class RxjsComponent {
   initAutocompleteMergeMapObservable() {
     return this.autoCompleteMergeMapControl.valueChanges
       .pipe(
+        takeUntilDestroyed(),
         filter((value: string | null): value is string => value !== null && value.length > 0),
         //debounceTime(300),
         //distinctUntilChanged(),  
@@ -121,20 +101,24 @@ export class RxjsComponent {
   initAutocompleteExhaustMapObservable() {
     return this.autoCompleteExhaustMapControl.valueChanges
       .pipe(
-        filter((value: string | null): value is string => value !== null && value.length > 0),
-        
+        takeUntilDestroyed(),
+        filter((value: string | null): value is string => value !== null && value.length > 0),        
         //debounceTime(300),
         //distinctUntilChanged(),    
         tap(() => this.selectedOperator = 'exhaustMap'),    
-        exhaustMap((searchQuery: string) => this.filterPokemonList(searchQuery, 'exhaustMap'))        
+        exhaustMap((searchQuery: string) => this.filterPokemonList(searchQuery, 'exhaustMap')),
+        catchError((error) => {
+          console.log('initAutocompleteExhaustMapObservable: ', error);
+          return of(error);
+        })        
       )
   }
 
   filterPokemonList(searchQuery: string, operatorName: string) {
     this.addLog(`Requête lancée avec ${this.selectedOperator} pour ${searchQuery}`, 'info');
     
-    let delay = 100;
-    if (this.isDelay()) {
+    let delay = 100;    
+    if (this.isDelay) {
       const delays = [5000, 3000, 200, 100];
       delay = delays[searchQuery.length];  // Choisir un délai basé sur la longueur
     }    
@@ -145,9 +129,10 @@ export class RxjsComponent {
           pokemon.name.toLowerCase().startsWith(searchQuery.toLowerCase())
         );
       }),
+      tap((list) => console.dir(list)),
       catchError((error) => {
         console.log('filterPokemonList', operatorName, error);
-        return EMPTY;
+        return of(error);
       })
     );
   }
