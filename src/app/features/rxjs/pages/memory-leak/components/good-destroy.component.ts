4c225db5b0e9d8c51272a3e6795e3cdf30f49a2f
pitchan <vincent.cueto@gmail.com>
2024-10-18 17:08:35 +0200

    preparing good destroy

--- src/app/features/rxjs/pages/memory-leak/components/good-destroy.component.ts
+++ src/app/features/rxjs/pages/memory-leak/components/good-destroy.component.ts
@@ -4,7 +4,15 @@ import { HighlightJsDirective } from 'ngx-highlight-js';
 import { CommonModule } from '@angular/common';
 import { MatRadioModule } from '@angular/material/radio';
 import { Pokemon } from '../../../../../core/model/pokemon.model';
+import { interval, of, switchMap, tap } from 'rxjs';
+import { PokemonApiService } from '../../../../../core/services/pokemon-api.service';
+import { RxjsService } from '../../../services/rxjs.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
+export enum MethodName {
+  signal = 'Signal',
+  observable = 'Observable'
+}
 
 @Component({
   selector: 'app-good-destroy',
@@ -14,5 +22,37 @@ import { Pokemon } from '../../../../../core/model/pokemon.model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GoodDestroyComponent {
-    constructor() {}
+
+  #pokemonApiService = inject(PokemonApiService);
+  #rxjsService = inject(RxjsService);
+
+  methodName = MethodName;
+  pokemon = toSignal(this.autoUpdatePokemon());
+  pokemon$ = this.autoUpdatePokemon();
+  selectedMethod = signal<string>('Observable');
+  
+
+  constructor() {}
+
+  selectMethod(event: any) {
+    this.selectedMethod.set(event.value);
+  }
+
+  autoUpdatePokemon() {
+    return of([]).pipe(
+      switchMap(() => this.getPokemonEveryTwoseconds()),
+      tap((pokemon) => {
+          this.#rxjsService.addLog('Request interval launched ' + pokemon.name, 'request')
+      }),    
+    );
+  }
+
+  getPokemonEveryTwoseconds(multiplier?: number) {
+    return interval(2000).pipe(
+        switchMap(() => {
+            const randomId = Math.floor(Math.random() * 898) + 1;
+            return this.#pokemonApiService.getPokemonById(randomId);
+        })
+    );
+}
 }
--- src/app/features/rxjs/pages/memory-leak/memory-leak.component.ts
+++ src/app/features/rxjs/pages/memory-leak/memory-leak.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, WritableSignal, inject, input } from '@angular/core';
 import { BadDestroyComponent } from './components/bad-destroy.component';
 import { MatButtonModule } from '@angular/material/button';
 import { LogPanelComponent } from '../../../../shared/components/log-panel/log-panel.component';
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { PokemonShowComponent } from '../../../../shared/components/pokemon-show/pokemon-show.component';
 import { RxjsService } from '../../services/rxjs.service';
 import { GoodDestroyComponent } from './components/good-destroy.component';
+import { Pokemon } from '../../../../core/model/pokemon.model';
 
 @Component({
   standalone: true,
@@ -22,7 +23,7 @@ export class MemoryLeakComponent {
   way = input.required<string>();
   
   componentExist = true;
-  pokemon = this.#rxjsService.currentPokemon;
+  pokemon: WritableSignal<Pokemon | null> = this.#rxjsService.currentPokemon;
 
   destroyComponent() {
     this.componentExist = false;
